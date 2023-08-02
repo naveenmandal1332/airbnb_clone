7293ac3e312429f65d83b7ef1b912c2514623bf6
naveenmandal1332 <nvnv1332@gmail.com>
2023-08-02 21:25:17 +0530

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 ">
+      <div className="space-y-4 text-xs text-gray-800 ">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Airbn.org: disaster relief housing</p>
+        <p>Combating discrimintaion</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOSTING</h5>
+        <p>Airbnb your home</p>
+        <p>AirCover for Hosts</p>
+        <p>Explore hosting resources</p>
+        <p>Vist our community forum</p>
+        <p>How to host responsibly</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>AirCover</p>
+        <p>Cancellation options</p>
+        <p>Supporting people with disabilities</p>
+        <p>Report a neighbourhood concern</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Banner from '../components/Banner.js';
 import SmallCard from '../components/SmallCard.js';
 import MediumCard from '../components/MediumCard.js';
 import LargeCard from '../components/LargeCard.js';
+import Footer from '../components/Footer.js';
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -18,10 +19,11 @@ export default function Home({ exploreData, cardsData }) {
       <Header />
       <Banner />
 
+      {/*This is Main section*/}
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        {/*Add Medium section card*/}
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {/** pull data from server */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map((item) => (
               <SmallCard
@@ -33,6 +35,8 @@ export default function Home({ exploreData, cardsData }) {
             ))}
           </div>
         </section>
+
+        {/*Add Medium section card*/}
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3">
@@ -42,6 +46,7 @@ export default function Home({ exploreData, cardsData }) {
           </div>
         </section>
 
+        {/*Add Large section card*/}
         <LargeCard
           img="https://links.papareact.com/4cj"
           title="The Greatest Outdoor"
@@ -49,6 +54,8 @@ export default function Home({ exploreData, cardsData }) {
           buttonText="Get Inspired"
         />
       </main>
+      {/*Add footer*/}
+      <Footer />
     </div>
   );
 }
